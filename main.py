58c31ba354ac5710ed4ae0680434a7d47d2fde52
sobralAllan <allan.sobraldasilva@gmail.com>
2022-06-20 21:20:43 -0300

    Primeiro Commit

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 import this
 import operacoes
+this.codigo = 0
 this.nome = ""
 this.telefone = ""
 this.endereco = ""
 this.data = ""
 this.dados = ""
+this.mensagem = ""
 
 pessoa = Flask(__name__) #Representando uma variável do tipo flask
 
@@ -18,6 +20,19 @@ def cadastrar():
         this.data     = request.form['tNovaData']
         this.dados    = operacoes.inserir(this.nome, this.telefone, this.endereco, this.data)
     return render_template('index.html', titulo='Página Principal', resultado=this.dados)
+
+@pessoa.route('/consultar.html', methods=['GET', 'POST'])
+def consultar():
+    if request.method == 'POST':
+        this.mensagem = operacoes.consultar()
+    return render_template('consultar.html', titulo='Consultar', dados=this.mensagem)
+
+@pessoa.route('/consultarCodigo.html', methods=['GET','POST'])
+def consultarCod():
+    if request.method == 'POST':
+        this.codigo = request.form['tCodigo']
+        this.mensagem = operacoes.consultarPorCodigo(this.codigo)
+    return render_template('consultarCodigo.html', titulo='Consultar por Código', consulta=this.mensagem)
 
 if __name__ == '__main__':
     pessoa.run(debug=True, port=5000)
--- operacoes.py
+++ operacoes.py
@@ -1,6 +1,7 @@
 import conexaoBD
 import mysql.connector
-
+import this
+this.msg = ""
 db_connection = conexaoBD.conexao()
 con = db_connection.cursor()
 
@@ -12,3 +13,33 @@ def inserir(nome, telefone, endereco, dataDeNascimento):
         return con.rowcount,"Inserido!"
     except Exception as erro:
         return erro
+
+def consultar():
+    try:
+        sql = "select * from pessoa"
+        con.execute(sql)
+
+        this.msg = ""
+        for (codigo, nome, telefone, endereco, dataDeNascimento) in con:
+            this.msg = this.msg + "Código: {} Nome: {} Telefone: {} Endereço: {} Data de nascimento: {}".format(codigo, nome, telefone, endereco, dataDeNascimento)
+        return this.msg
+    except Exception as erro:
+        return erro
+
+def consultarPorCodigo(cod):
+    try:
+        sql = "select * from pessoa where codigo = '{}'".format(cod)
+        con.execute(sql)
+
+        this.msg = ""
+        this.msg = "Nenhum dado encontrado!"
+        for(codigo, nome, telefone, endereco, dataDeNascimento) in con:
+            if int(codigo) == int(cod):
+                this.msg = "Código: {} Nome: {} Telefone: {} Endereço: {} Data de nascimento: {}".format(codigo, nome, telefone, endereco, dataDeNascimento)
+                return this.msg
+        return this.msg
+    except Exception as erro:
+        return erro
+
+
+
